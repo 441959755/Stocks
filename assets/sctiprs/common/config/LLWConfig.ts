b4ea4494a0b5c0f88dd3c0d1b1d58a733389a231
441959755 <441959755@qq.com>
2022-04-21 15:04:01 +0800

    2.4.8
    cocos版本跟新2.4.8

--- assets/sctiprs/common/config/LLWConfig.ts
+++ assets/sctiprs/common/config/LLWConfig.ts
@@ -6,11 +6,11 @@ const GAMEID = 0;
 
 //const PLATTYPE = PlatDefine.PLAT_IOS;
 
-// const PLATTYPE = PlatDefine.PLAT_WEB;
-// const FROM = pb.AppFrom.Website3th;
+const PLATTYPE = PlatDefine.PLAT_WEB;
+const FROM = pb.AppFrom.Website3th;
 
-const PLATTYPE = PlatDefine.PLAT_WECHAT;
-const FROM = pb.AppFrom.WeChatMinProgram;
+// const PLATTYPE = PlatDefine.PLAT_WECHAT;
+// const FROM = pb.AppFrom.WeChatMinProgram;
 
 //const LoginURL = 'http://test.cgdr168.com';
 const LoginURL = 'https://www.cgdr168.com';
