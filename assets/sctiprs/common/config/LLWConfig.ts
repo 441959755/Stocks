f98c8d552117d49f522378b9009df48796d1b895
DESKTOP-9IBV7RU\Administrator <441959755@qq.com>
2022-05-09 17:12:57 +0800

    Revert "Update LLWConfig.ts"
    This reverts commit ff9ef0eba9b4a7be1cd625be98a26d0462864e79.

--- assets/sctiprs/common/config/LLWConfig.ts
+++ assets/sctiprs/common/config/LLWConfig.ts
@@ -12,6 +12,7 @@ const GAMEID = 0;
 const PLATTYPE = PlatDefine.PLAT_WECHAT;
 const FROM = pb.AppFrom.WeChatMinProgram;
 
+// 
 //const LoginURL = 'http://test.cgdr168.com';
 const LoginURL = 'https://www.cgdr168.com';
 
